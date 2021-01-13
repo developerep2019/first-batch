33fb3031862dbab7044fdc00e2fde7970612ff58
developerep2019 <developerep2019@gmail.com>
2021-01-13 22:19:18 +0600

    code of first class

---
+++ index.js
@@ -0,0 +1,33 @@
+/*
+ 
+* Variable = variable
+ * Array
+ * Objects
+ * Function
+ * Data Types
+ 
+ */
+
+
+/*
+3 ways to declare a variable
+
+var -> before ES6
+let / Const -> After e6
+*/
+
+
+/*
+Conditions for declare a variable : 
+* can't declare a variable name starting with a number
+* variable name should be meaningful
+* we can't declare a variable name, which is preserved by javascript
+
+types of writing variable names :
+camelCase
+PascalCase
+
+Let vs Const
+1. the value of let can be reassigned but const can't
+
+*/
