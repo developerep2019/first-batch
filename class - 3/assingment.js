2117ad6f5300f9623154b4ce059238b4e010a76c
developerep2019 <developerep2019@gmail.com>
2021-01-23 23:46:24 +0600

    adding assignment operator

---
+++ class - 3/assingment.js
@@ -0,0 +1,25 @@
+//Javascript Assignment operators......
+
+// (++) , (--);
+
+
+
+/*
+
+increment operator = ++
+
+and if we think a variable named x;
+++x equals to x = x+1
+*/
+
+let x = 5;
+
+/*
+
+(x = x + 10) and (x += 10) they are same
+(x = x - 10) and (x -= 10) they are same
+(x = x * 10) and (x *= 10) they are same
+(x = x / 10) and (x /= 10) they are same
+
+
+*/
---
+++ class - 3/index.js
@@ -0,0 +1,34 @@
+// In Javascript We have operators for creating expressions and performing some algorithms...
+
+//Arithmetic Operators
+// Assignment Operators
+//Comparison operators
+// Logical Operators
+
+
+//Arithmetic Operators
+// +
+// -
+// *
+// /
+// %
+// **
+
+//Increment (++)
+
+//Decrement (--)
+
+// let x = 20;
+
+// let y = 10;
+
+// console.log(x + y);
+// console.log(x - y);
+// console.log(x * y);
+// console.log(x / y);
+// console.log(x % y);
+// console.log(x ** y);
+
+// console.log(x--);
+// console.log(x);
+
