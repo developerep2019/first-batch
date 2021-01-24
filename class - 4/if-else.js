364547c6798931061eb7f521e8e45106f8cab58f
developerep2019 <developerep2019@gmail.com>
2021-01-24 23:47:08 +0600

    class 04 done

---
+++ class - 4/if-else.js
@@ -0,0 +1,16 @@
+//for example we have a time
+// if the time is under 6am to 12pm then we will show the user : good morning;
+//if the time is under 12pm to 6pm then we will show the user : good afternoon;
+//otherwise : good evening;
+
+// let time = 20;
+
+// if(time > 6 && time < 12) {
+//     console.log("good morning")
+// }
+// else if(time > 12 && time < 18) {
+//     console.log("good afternoon");
+// }
+// else {
+//     console.log("good Evening");
+// }
---
+++ class - 4/switch-case.js
@@ -0,0 +1,19 @@
+//for example we have a website like facebook... and we have account system there. 
+// if we has a data like what the role of the user is..
+//if the data is guest we will show "welcome guest"
+//if the data is a mod, we will show "welcome moderator"
+//otherwise we will show "unknown user";
+
+const role = 'admin';
+
+switch(role) {
+    case 'guest' : 
+        console.log('welcome guest')
+        break;
+
+    case 'mod' : 
+        console.log("welcome Moderator");
+        break;
+    default : 
+        console.log("unknown user");
+};
