07b385b2a3063429a73cc875e325fc4c0eec73c4
developerep2019 <developerep2019@gmail.com>
2021-01-26 00:49:33 +0600

    while loop done

---
+++ class - 5/readme.txt
@@ -0,0 +1,15 @@
+//For loop syntax : 
+
+for(variable; condition; steps) {
+    // your code block here
+}
+
+//while loop structure
+
+let variableName = 0;
+while(condition) {
+    //your code block here
+
+
+    variableName++
+}
---
+++ class - 5/while.js
@@ -0,0 +1,14 @@
+//suppose your manager told you to write "I love my company" for 1000 times;
+// now write "I love my company " in a productive way
+// and also think like a programmer
+// but this time you must have to use while loop
+
+// for(let i = 0; i < 1000; i++) {
+//     document.write('I love my company');
+// };
+
+let i = 0;
+while(i < 1000) {
+    document.write('I love my company from while <br>')
+    i++;
+}
