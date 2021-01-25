e6f4273ceb4f38ccc5896963394fe5847543d0a5
developerep2019 <developerep2019@gmail.com>
2021-01-26 00:38:35 +0600

    for-loop done

---
+++ class - 5/for.js
@@ -0,0 +1,11 @@
+//suppose your manager told you to write "I love my company" for 1000 times;
+// now write "I love my company " in a productive way
+// and also think like a programmer
+
+// for(/* variable; times of looping ; how many ; step*/  let i = 0; i < 1000; i++) {
+    
+//    i % 2 === 0 && console.log(i + 2)
+
+
+
+// }
